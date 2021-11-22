0f44257d13899c861babf1fee1847002d8a4ff01
cedric-h <cedhut02@gmail.com>
2021-11-22 18:46:14 -0500

    switch to conversations.history api

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,81 @@
-/// <reference types="@slack/types" />
-/// <reference types="@slack/bolt" />
 import { token, signingSecret } from "./auth.json";
 import { App } from '@slack/bolt';
-import { Match } from "@slack/web-api/dist/response/SearchMessagesResponse";
+import { Reaction } from "@slack/web-api/dist/response/ConversationsHistoryResponse";
 
 // Initializes your app with your bot token and signing secret
 const app = new App({ token: token.bot, signingSecret });
+const ids = { ship: "C0M8PUPU6" };
+
+const sum = (arr: number[]) => arr.reduce((a, x) => a + x);
+
+/* normalizes an ID */
+function isUserId(id: string) { return !!id.match(/^<@([a-zA-Z0-9]+)(?:\|[A-Za-z]+)?>$/); }
 
 (async () => {
   // Start your app
   await app.start(3000);
 
-  function threadTsFromPermalink(permalink: string) {
-    const m = permalink.match(/thread_ts=([0-9.]*)/);
-    return m ? m[1] : m;
-  }
-
-  // The echo command simply echoes on command
   app.command('/sc', async ({ command, ack, respond }) => {
-    // Acknowledge command request
     await ack();
 
-    let ships: Match[] = [];
-    let cursor: string = "*";
-    while (cursor != "") {
-      const res = await app.client.search.messages({
-        token: token.user,
-        page: 1000,
+    console.log(command.text);
+    const user = (command.text.length > 0) ? command.text : `<@${command.user_id}>`;
+    if (!isUserId(user)) return await respond(`Expected user id, not ${user}!`);
+
+    const shipmoji: Reaction[] = [];
+    for (let res, cursor; !res || res.has_more; cursor = res.response_metadata!.next_cursor!) {
+      console.log(cursor);
+      res = await app.client.conversations.history({
+        channel: ids.ship,
+        limit: 1000,
         cursor,
-        query: "in:<#C0M8PUPU6|ship> from:" + command.user_id,
-        sort: "timestamp",
-        sort_dir: "desc",
       });
-      if (!res.ok) await respond("" + res?.error);
+      if (!res.ok) return await respond("" + res.error);
 
-      cursor = res.response_metadata!.next_cursor!;
-
-      ships = ships.concat((res.messages!.matches ?? []).filter(m => {
-        const threadTs = threadTsFromPermalink(m.permalink!);
-        return !threadTs || (threadTs == m.ts);
-      }));
+      for (const msg of res.messages!)
+        if (msg.reactions && msg.reactions.length > 0)
+          shipmoji.push(msg.reactions.reduce((a, x) => (x.count! > a.count!) ? x : a));
     }
 
-    await respond(ships.length + " messages in #ship");
-  });
+    if (shipmoji.length == 0)
+      return await respond(
+        "looks like you haven't shipped anything yet, " +
+        "or your ships haven't received any reactions.\n\n" +
+        `post something cool you've made in ${ids.ship},` +
+        "and the scales may yet tip in your favor!"
+      );
 
+    /* ships = number of ships with this moji */
+    type MojiVariant = { name: string, ships: number, sum: number };
+    const mojivariants: MojiVariant[] = (() => {
+      const map: Map<string, number[]> = shipmoji.reduce(
+        (ret, {name, count}) => {
+          if (ret.has(name))
+            ret.get(name).push(count);
+          else
+            ret.set(name, [count]);
+          return ret;
+        },
+        new Map()
+      );
+      const ret = [...map.entries()].map(([name, perShip]) => {
+        return { name, ships: perShip.length, sum: sum(perShip) };
+      });
+      //ret.sort((a, b) => b.sum - a.sum);
+      ret.sort((a, b) => b.ships - a.ships);
+      return ret;
+    })();
+    const totalMaxReacts = sum(shipmoji.map(r => r.count!));
+
+    await respond((() => {
+      // let txt = `<@${user}> has received ${totalMaxReacts} from`;
+      let txt = `y'all have received ${totalMaxReacts} most popular reactions from`;
+      txt += ` across ${shipmoji.length} <#${ids.ship}>s`;
+      for (const {name, sum, ships} of mojivariants)
+        txt += `\n:${name}: ${sum} across ${ships} ships`;
+      console.log(txt);
+      return txt;
+    })());
+  });
   console.log('⚡️ Bolt app is running!');
 })();
